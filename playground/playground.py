8755e7f9fd9606b6488dde2cb801596ee803b3f0
BlueDragon23 <aidan_goldthorpe@outlook.com>
2016-11-15 20:14:30 +0100

    Fixed the axes on graphs, made it more responsive. Added a purchases skeleton for use with real data

--- playground/playground.py
+++ playground/playground.py
@@ -1,4 +1,5 @@
 import os
+import pandas
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
@@ -6,9 +7,28 @@ from flask import Flask, request, session, g, redirect, url_for, abort, \
 app = Flask(__name__)
 app.config.from_object(__name__)
 
+
+def load_purchases():
+    purchases = pandas.read_csv("../PurchaseHistory.csv")
+    return purchases
+
+
 @app.route('/')
 def show_shapes():
     return render_template('index.html')
 
-if __name__=="__main__":
-    app.run()
+
+@app.route('/charts')
+def show_charts():
+    return render_template('graphs.html')
+
+
+# consider making this authenticated
+@app.route('/purchases')
+def show_purchases():
+    purchases = load_purchases()
+    return render_template('purchases.html', purchases=purchases)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
